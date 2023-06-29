2f4ac2afcf976154e4711dd92aa5aa60e01dbb58
YuliaGrigorieva <miss000hara@gmail.com>
2023-06-29 21:00:39 +0300

    [video_call}: update android code for null safety

--- video_call/android/app/src/main/java/com/voximplant/flutter/videocall/MainActivity.java
+++ video_call/android/app/src/main/java/com/voximplant/flutter/videocall/MainActivity.java
@@ -1,8 +1,6 @@
 package com.voximplant.flutter.videocall;
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity { }
+public class MainActivity extends FlutterActivity {
+}
